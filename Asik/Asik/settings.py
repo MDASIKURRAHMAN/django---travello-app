05d69d6aad54abe07a689efce4090395782e960b
MDASIKURRAHMAN <mdasikur1.25@gmail.com>
2020-09-30 12:39:29 +0600

    sending email when signup

--- Asik/Asik/settings.py
+++ Asik/Asik/settings.py
@@ -128,4 +128,12 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-                                                                                                                                                                                                                                                                                   
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER ="mdasikur1.25@gmail.com"
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT =587
+EMAIL_USE_TLS =True
+EMAIL_HOST_PASSWORD = "mypassword"
+
+                                                                                                                                                                                                                                                                                  
--- Asik/accounts/views.py
+++ Asik/accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User,auth
+from django.conf import settings
+from django.core.mail import send_mail
+
 def register(request):
     if request.method=='POST':
 
@@ -16,19 +19,28 @@ def register(request):
             if User.objects.filter(username=username).exists():
                 messages.info(request,"username taken")
                 return redirect(request,'register')
-            elif User.objects.filter(email=email).exists():
+        
+            elif User.objects.filter(last_name=last_name).exists():
                 messages.info(request,"email taken")
                 return redirect(request,'register')
+            
             else:
             
                 user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, email=email, password=password)
                 user.save()
+                send_mail(
+                    'Registration Succesfull',
+                    'you have succesfully registered into our website',
+                    'mdasikur1.25@gmail.com',
+                    ['mdasikur1.25@gmail.com'],
+                    )
                 print("user created")
+                return redirect('login')
             return redirect('login')
             
 
         else:
-            print ("password not matching")
+            messages.info(request,"password not matching")
             return redirect('register')
         return redirect('/')
     else:
